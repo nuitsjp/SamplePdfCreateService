eda7f5cfac3247ee9864665102cc62e38e7f5043
Atsushi Nakamura <atsushi_a_nakamura@jp.ricoh.com>
2020-09-03 16:43:20 +0900

    不要ファイルの削除

--- PdfCreateService/PdfCreateService/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace PdfCreateService.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- PdfCreateService/PdfCreateService/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace PdfCreateService.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
